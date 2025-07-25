2e5503e4a412de22ce3a45614eae7fdae18712aa
zunemoe <2897539+zunemoe@users.noreply.github.com>
2025-07-25 13:10:27 +1200

    webpack config fixes

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,17 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isNetwork = env && env.network;
+  const isProduction = argv.mode === "production";
   return {
-    mode: "development", // Set the mode to development
+    mode: isProduction ? "production" : "development",
     entry: "./src/index.js", // Entry point for the application
     output: {
       filename: "main.js",
       path: path.resolve(__dirname, "dist"), // Output directory
       clean: true, // Clean the output directory before emit
-      publicPath: "/",
+      publicPath: isProduction ? "/top-todo-list/" : "/",
     },
-    devtool: "eval-source-map",
+    devtool: isProduction ? "source-map" : "eval-source-map",
     devServer: {
       host: isNetwork ? "0.0.0.0" : "localhost",
       port: 8080, // Or any open port you prefer
