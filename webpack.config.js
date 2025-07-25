e778b22e11e73ea30fb692ac454f440bf023e5d7
zunemoe <2897539+zunemoe@users.noreply.github.com>
2025-07-25 12:23:13 +1200

    add localhost and network config

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,50 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development", // Set the mode to development
-  entry: "./src/index.js", // Entry point for the application
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"), // Output directory
-    clean: true, // Clean the output directory before emit
-    publicPath: "/",
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    host: "0.0.0.0", // Accept connections from any network interface
-    port: 8080, // Or any open port you prefer
-    allowedHosts: "all", // Accept requests from any hostname
-    watchFiles: ["./src/template.html"], // Keep this line
-    static: {
-      directory: path.resolve(__dirname, 'dist'), // Serve static content from dist
+module.exports = (env, argv) => {
+  const isNetwork = env && env.network;
+  return {
+    mode: "development", // Set the mode to development
+    entry: "./src/index.js", // Entry point for the application
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"), // Output directory
+      clean: true, // Clean the output directory before emit
+      publicPath: "/",
     },
-    hot: true,
-    liveReload: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html", // Template HTML file
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+    devtool: "eval-source-map",
+    devServer: {
+      host: isNetwork ? "0.0.0.0" : "localhost",
+      port: 8080, // Or any open port you prefer
+      allowedHosts: "all", // Accept requests from any hostname
+      watchFiles: ["./src/template.html"], // Keep this line
+      static: {
+        directory: path.resolve(__dirname, "dist"), // Serve static content from dist
       },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+      hot: true,
+      liveReload: true,
+      open: !isNetwork, //Only open browser for localhost
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/template.html", // Template HTML file
+      }),
     ],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+  };
 };
